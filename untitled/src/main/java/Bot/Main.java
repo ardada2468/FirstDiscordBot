34d536428fc0d81c444943cbc13357d9ae86cd1e
cupsmart1 <37542991+cupsmart1@users.noreply.github.com>
2020-08-31 11:47:43 -0400

    Add files via upload

---
+++ untitled/src/main/java/Bot/Main.java
@@ -0,0 +1,25 @@
+package Bot;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Insert your bot's token here
+        String token = "NzUwMDE2ODc3MzQ2MTYwNjYw.X00Zpg.rZ8P9X8pXdXh4S-9TCGKWbV5q8c";
+
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessage().getContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+
+        // Print the invite url of your bot
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+    }
+
+}
